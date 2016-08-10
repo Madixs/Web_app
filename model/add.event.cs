7cbf3ee9bf6d766ccceec9db106c16ca86581971
Madixs <Krysti_81@yahoo.pl>
2016-08-10 14:34:52 +0200

    addevent

--- model/add.event.cs
+++ model/add.event.cs
@@ -8,6 +8,12 @@ namespace model
 {
     class AddEvent
     {
-
+       public int Id_Wydarzenia { get; set; }
+       public string imie { get; set; }
+       public string naziwsko { get; set; }
+       public int nr_telefonu { get; set; }
+       public string email { get; set; }
+       public int nr_pokoju { get; set; }
+               
     }
 }
