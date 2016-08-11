ef10fdabd39235add8d509431318bf5108d4dd82
hubertch <h.checinski@wp.pl>
2016-08-11 16:55:01 +0200

    poprawiona nazwa i zintegrowana z baza, repository działają

---
+++ model/DisplayData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class DispalyData
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string phoneNumber { get; set; }
+        public string email { get; set; }
+        public int lroomNumber { get; set; }
+        public int eventNameer { get; set; }
+        public string eeventDate { get; set; }
+        public string signUpDate { get; set; }
+
+    }
+}
---
+++ model/SaveData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class SaveData
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string phoneNumber { get; set; }
+        public string email { get; set; }
+        public int roomNumber { get; set; }
+        public int eventID { get; set; }
+    }
+}
