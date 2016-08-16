ad757e677bce263cf5dc21aaac2073342b71948c
hubertch <h.checinski@wp.pl>
2016-08-16 16:02:05 +0200

    sad
    das

---
+++ Resolver/RepositoryBDResolver.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Repository;
+
+namespace Resolver
+{
+    public class RepositoryBDResolver
+    {
+        public static IRepository Get()
+        {
+            return new RepositoryDB();
+        }
+    }
+}
